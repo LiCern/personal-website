c482669e1223df3d0f37f12f57e5d359f0ed0071
LiCern <lisacernilogar@me.com>
2020-04-17 11:00:01 +0100

    wrote forward and backward function, added autoloop function

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 const carouselSlide = document.querySelector('.carousel-slide');
 const carouselImages = document.querySelectorAll('.carousel-slide img');
+const nextButton = document.querySelector('#n')
+
+//counter 
+
+let counter = 0;
+const scrollWidth = carouselImages[0].clientWidth;
+
+//toggle automatically through
+
+//transition
+
+const transitionSlideForward = () => {
+    carouselSlide.style.transition = 'transform 1s ease-in';
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-scrollWidth * counter) + 'px)'; 
+}
+
+const transitionSlideBack = () => {
+    carouselSlide.style.transition = 'transform 1s ease-in';
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-scrollWidth * counter) + 'px)';
+}
+
+
+
+
+function autoLoop() {
+    setInterval(transitionSlideForward, 5000);
+    
+  }  
+  autoLoop();
 
 
 
