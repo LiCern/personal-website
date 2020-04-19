06af3c90dd6b925590073959b83907c1af2bcad4
LiCern <lisacernilogar@me.com>
2020-04-19 14:34:01 +0100

    added translation in order to start at the first image and not the lastImageCopy when the page loads

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+//quesry selectors for the carousel containter and carousel images
 const carouselSlide = document.querySelector('.carousel-slide');
 const carouselImages = document.querySelectorAll('.carousel-slide img');
-const nextButton = document.querySelector('#n')
 
 //counter 
 
-let counter = 0;
+let counter = 1;
 const scrollWidth = carouselImages[0].clientWidth;
+carouselSlide.style.transform = 'translateX(' + (-scrollWidth * counter) + 'px)'; //in order to start at the first image and not the lastImageCopy when the page loads
 
-//transition
+//Transition functions (a.k.a the backbone of the carousel)
 
 const transitionSlideForward = () => {
     carouselSlide.style.transition = 'transform 1s ease-in';
@@ -20,6 +21,22 @@ const transitionSlideBack = () => {
     counter--;
     carouselSlide.style.transform = 'translateX(' + (-scrollWidth * counter) + 'px)';
 }
+
+
+//Toggle automatically looping through all images with play/pause functions
+
+let autoLoop = setInterval(transitionSlideForward, 5000);   
+
+const startLoop = () => {
+    autoLoop = setInterval(transitionSlideForward, 5000);
+}
+
+const stopLoop = () => {
+    clearInterval(autoLoop);
+}
+
+//Stop the autoLoop from going forward at its normal interval times when the back/forward buttons are clicked 
+//Reset the interval instead and ensure smooth transition
 
 const forwardResetTimer = () => {
     transitionSlideForward();
@@ -34,22 +51,23 @@ const backwardResetTimer = () => {
 
 }
 
-// toggle automatically through and play/pause functions 
 
-let autoLoop = setInterval(transitionSlideForward, 5000);   
+//Jumping from image clones to the original image and removing the the transition, thus creating the illusion of an infinite loop
 
-const stopLoop = () => {
-    clearInterval(autoLoop);
+if (carouselImages[counter].id === "lastImagecopy") {
+    carouselSlide.style.transition = 0;
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = 'translateX(' + (-scrollWidth * counter) + 'px)'; 
 }
 
-const startLoop = () => {
-    autoLoop = setInterval(transitionSlideForward, 5000);
-}
+
+
+//Buttons with related functions
 
 document.getElementById("play-button").addEventListener("click", startLoop); //if clicked, restarts loop
 document.getElementById("pause-button").addEventListener("click", stopLoop); //if clicked, clears interval and pauses carousel
-document.getElementById("next-button").addEventListener("click", transitionSlideForward);
-document.getElementById("back-button").addEventListener("click", transitionSlideBack);
+document.getElementById("next-button").addEventListener("click", forwardResetTimer);
+document.getElementById("back-button").addEventListener("click", backwardResetTimer);
 
 
 
